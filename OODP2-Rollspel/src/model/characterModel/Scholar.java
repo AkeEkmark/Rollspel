40827b947a2b5c393dfba05d9b1b2c3232ed28c4
nbt12awt <nbt12awt@09814-STUD-D.HIG-AD.hig.se>
2016-03-21 13:50:42 +0100

    3 klasser

---
+++ OODP2-Rollspel/src/model/characterModel/Scholar.java
@@ -0,0 +1,13 @@
+package model.characterModel;
+
+public class Scholar extends Hero{
+	public Scholar (String name) {
+		super(name);
+		super.strength = 10;
+		super.Agility = 10;
+		super.intelligence = 15;
+		super.hitpoints = 30;
+		super.maxHitpoints = 30;
+		super.inventory = new Inventory();
+	}
+}
